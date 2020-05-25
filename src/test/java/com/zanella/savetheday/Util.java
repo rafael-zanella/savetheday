8ec83bd5cdaf7b662c6c80f17038b096b3f91d70
Rafael S Zanella <rafaels.zanella@gmail.com>
2020-05-25 20:31:24 +0000

    refactor(ONGControllerTest, Util): create method to convert dto to json.

---
+++ src/test/java/com/zanella/savetheday/Util.java
@@ -0,0 +1,16 @@
+package com.zanella.savetheday;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class Util {
+
+    public static String ObjectToJsonStr(Object object) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper.writeValueAsString(object);
+    }
+}
--- src/test/java/com/zanella/savetheday/controllers/ONGControllerTest.java
+++ src/test/java/com/zanella/savetheday/controllers/ONGControllerTest.java
@@ -1,5 +1,9 @@
 package com.zanella.savetheday.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.zanella.savetheday.Util;
 import com.zanella.savetheday.dto.ONGDto;
 import com.zanella.savetheday.entities.ONG;
 import com.zanella.savetheday.services.ONGService;
@@ -11,7 +15,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -59,26 +67,18 @@ class ONGControllerTest {
 
     @Test
     void saveNewONG() throws Exception {
-        StringBuilder json = new StringBuilder();
-        json.append("{ ");
-        json.append("\"id\" : 5, ");
-        json.append("\"nome\" : \"ong 3\", ");
-        json.append("\"dataFundacao\": \"2018-05-13\", ");
-        json.append("\"cnpj\" : \"02499010000149\", ");
-        json.append("\"telefone\" : \"5133331146\", ");
-        json.append("\"senha\" : \"zxczxczxc\", ");
-        json.append("\"email\" : \"rafael@email.com\"");
-        json.append(" }");
-
-        ONG ong1 = new ONG(1, "ong 3", LocalDate.of(2018, 05, 13), "02499010000149", "5133331146", "email@email.com", "123456789",null);
+        ONG ong1 = new ONG(1, "ong 3", LocalDate.of(2018, 5, 13), "02499010000149", "5133331146", "email@email.com", "123456789",null);
+        ONGDto dto = new ONGDto("ong 3", LocalDate.of(2018, 5, 13), "02499010000149", "5133331146", "email@email.com", "123456789");
+        String data = Util.ObjectToJsonStr(dto);
 
         when(this.service.add(ArgumentMatchers.any(ONGDto.class))).thenReturn(ong1);
 
         mockMvc.perform(post("/ong/")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(json.toString()))
+                .content(data))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.senha").doesNotExist());
     }
+
 }
